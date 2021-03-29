22e7977aae158849921c31d018b582e17e07cfab
dmitars <dmitriy.tarasienko@gmail.com>
2021-03-29 21:51:09 +0300

    feat: create exe file

--- src/sample/MainWindow.java
+++ src/sample/MainWindow.java
@@ -1,12 +1,11 @@
-package com.company;
+package sample;
 
+import com.company.DrawPanel;
 import com.company.calculators.CalculatorFactory;
 import com.company.calculators.CalculatorType;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 public class MainWindow extends JFrame {
